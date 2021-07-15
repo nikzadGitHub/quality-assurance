866900b27557ac7e85820309884ba9174fa53f86
nasernikzad <n.nikzad@pomtech.com>
2021-07-15 08:33:34 +0430

    changes

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -47,8 +47,8 @@ suite('Unit Tests', function () {
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
-      assert.fail({ a: '1', b: 5 }, { b: 5, a: '1' }, "keys order doesn't matter");
-      assert.fail({ a: [5, 6] }, { a: [6, 5] }, "array elements position does matter !!");
+      assert.notDeepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "keys order doesn't matter");
+      assert.deepEqual({ a: [5, 6] }, { a: [6, 5] }, "array elements position does matter !!");
     });
   });
 
